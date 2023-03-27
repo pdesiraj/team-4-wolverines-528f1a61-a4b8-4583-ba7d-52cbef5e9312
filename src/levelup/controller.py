857070fc2bb1c15ef06507e88ba63382606829bf
Erin Perry <georeri@iit.edu>
2023-03-27 12:51:11 -0400

    Initialize syncpoint-atdd-wired

---
+++ src/levelup/controller.py
@@ -0,0 +1,49 @@
+import logging
+from dataclasses import dataclass
+from levelup.character import Character, DEFAULT_CHARACTER_NAME, InvalidMoveException
+from levelup.map import Direction, GameMap
+from levelup.position import Position
+
+
+@dataclass
+class GameStatus:
+    move_count: int = 0
+    running: bool = False
+    current_position: Position = None
+
+    def __str__(self):
+        return f"Moved {self.move_count} times, currently on position {self.current_position}"
+
+
+class CharacterNotFoundException(Exception):
+    pass
+
+
+class GameController:
+    status: GameStatus
+    character: Character
+
+    def __init__(self):
+        self.status = GameStatus()
+        self.character = None
+        self.map = GameMap()
+
+    def start_game(self):
+        if hasattr(self, "character"):
+            self.character.enter_map(self.map)
+            self.status.current_position = self.character.position
+            self.set_character_position(self.map.starting_position)
+        else:
+            raise CharacterNotFoundException("Character not found")
+
+    def create_character(self, character_name: str) -> None:
+        self.character = Character(character_name)
+
+    def move(self, direction: Direction) -> None:
+        pass
+
+    def set_character_position(self, position: Position):
+        self.character.position = position
+
+    def get_total_positions(self):
+        return self.map.position_count
---
+++ tests/robot/GameControllerLibrary.py
@@ -0,0 +1,24 @@
+from levelup.controller import GameController
+
+
+class GameControllerLibrary:
+    def initialize_controller(self):
+        pass
+
+    def create_character_with_name(self, charactername):
+        pass
+
+    def character_name_should_be(self, expected):
+        pass
+
+    def number_of_map_positions_should_be(self, expected):
+        raise AssertionError("Not implemented")
+
+    def starting_X_coordinate_should_be(self, expected):
+        raise AssertionError("Not implemented")
+
+    def starting_Y_coordinate_should_be(self, expected):
+        raise AssertionError("Not implemented")
+
+    def starting_move_count_should_be(self, expected):
+        raise AssertionError("Not implemented")
